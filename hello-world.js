1d7a84545c689652ba7fb984cf3099e0d6b43010
GustavoMargotti <gustavo.cesconetto2@gmail.com>
2025-03-24 20:27:21 -0300

    Atualização

--- hello-world.js
+++ hello-world.js
@@ -4,9 +4,23 @@ const messageP = document.getElementById('message');
 nameInput.onkeyup = (event) => {
     if (nameInput.value === '') {
         messageP.innerText = '';
+    }else {
+        if (event.keyCode == 13)
+            helloWorld();
     }
 }
 
+document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode == 27) {
+        nameInput.value = "";
+    }
+})
+
 const helloWorld = () => {
     messageP.innerText = `Hello World, ${nameInput.value}!`;
+}
+
+function emptyFields(){
+    nameInput.value = '';
+    messageP.innerText = '';
 }
